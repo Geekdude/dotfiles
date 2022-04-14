cf9013f8133fb8b0e16d7bbf021e6bb464520147
Aaron Young <ayoung3210@gmail.com>
2022-04-14 01:10:33 -0400

    Added curl.

--- basic_apt_installs.sh
+++ basic_apt_installs.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt upgrade
-sudo apt install git git-gui vim-gtk3 tmux fish build-essential meld tree exuberant-ctags \
+sudo apt install git git-gui vim-gtk3 tmux fish build-essential meld tree exuberant-ctags curl \
    valgrind kcachegrind ncdu htop glances \
    python3 python3-venv python3-pip \
    texlive-full pdf-presenter-console pandoc graphviz ripgrep rename \
